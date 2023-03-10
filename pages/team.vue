cf47da8c2541e812e3f461cce4b32bce21dede2f
maxnth <maximilian.noeth@protonmail.com>
2023-03-10 11:37:33 +0100

    update team page

--- pages/team.vue
+++ pages/team.vue
@@ -164,7 +164,7 @@
             <div class="relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
               <div class="absolute inset-0">
                 <img class="h-full w-full rounded-xl object-cover shadow-xl shadow-black/40"
-                     src="https://www.sfb-episteme.de/_media/mitarbeitende/o_haehnke.jpg?width=350" alt="" />
+                     src="/team/eliza_hähnke.jpg" alt="" />
               </div>
               <div class="absolute inset-0 h-full w-full rounded-sxl bg-black/80 px-12 text-center text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden]">
                 <div class="flex min-h-full flex-col items-center justify-center">
