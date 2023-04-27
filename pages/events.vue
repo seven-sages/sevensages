f3e6c058a48353df9f7dc459b2a7c8e00ce5db4c
maxnth <maximilian.noeth@protonmail.com>
2023-04-27 20:14:42 +0200

    fix dark mode class

--- pages/events.vue
+++ pages/events.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-white dark:bg-gray-800 py-6 sm:py-8 lg:py-12">
     <div class="mx-auto max-w-screen-md px-4 md:px-8">
-      <h1 class="mb-4 text-center text-2xl font-bold text-gray-800 sm:text-3xl md:mb-6">
+      <h1 class="mb-4 text-center text-2xl font-bold text-gray-800 dark:text-white sm:text-3xl md:mb-6">
         Events
       </h1>
 
