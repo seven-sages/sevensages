19e4afbad5ee692f96ce94de0122886fe1a6c002
maxnth <maximilian.noeth@protonmail.com>
2023-06-19 09:30:59 +0200

    fix(navbar): fix styling of navbar links

--- components/layout/NavbarLink.vue
+++ components/layout/NavbarLink.vue
@@ -8,7 +8,7 @@ defineProps<{
 <template>
   <NuxtLink
     :to="link"
-    class="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700 dark:text-white dark:hover:text-indigo-500"
+    class="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-600 active:text-indigo-700 dark:text-white dark:hover:text-indigo-400"
   >
     {{ name }}
   </NuxtLink>
