ec47fe71f618d331e7ebfe62eee0809444b4b3e6
maxnth <maximilian.noeth@protonmail.com>
2023-06-20 09:35:45 +0200

    feature(materials): pull material data from CMS

--- pages/materials.vue
+++ pages/materials.vue
@@ -1,13 +1,30 @@
 <script setup lang="ts">
-const { getSingletonItem } = useDirectusItems();
+const { getSingletonItem, getItemById } = useDirectusItems();
 interface IMaterials {
   id?: string;
   Header: string;
+  Material: object[];
+}
+
+interface IMaterial {
+  Content: string;
+  Link: string;
 }
 
 const materials = await getSingletonItem<IMaterials>({
   collection: "Materials",
+  params: {
+    fields: ["Header", "Material.Material_id"],
+  },
 });
+const material = [];
+for (const entry of Object.values(materials.Material)) {
+  const item = await getItemById<IMaterial[]>({
+    collection: "Material",
+    id: entry.Material_id,
+  });
+  material.push(item);
+}
 </script>
 <template>
   <div class="bg-white py-6 dark:bg-gray-800 sm:py-8 lg:py-12">
@@ -18,7 +35,15 @@ const materials = await getSingletonItem<IMaterials>({
         {{ materials.Header }}
       </h1>
 
-      <p class="mb-6 text-gray-500 dark:text-gray-200 sm:text-lg md:mb-8"></p>
+      <ul
+        class="mb-6 list-inside list-disc text-gray-500 dark:text-gray-200 sm:text-lg md:mb-8"
+      >
+        <li v-for="event in material" :key="event.id">
+          <a :href="event.Link" target="_blank">
+            {{ event.Content }}
+          </a>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
