4ab767207bb3da5bb83e8f600786e745161e4cd0
maxnth <maximilian.noeth@protonmail.com>
2024-06-21 12:18:48 +0200

    feature: connect database page to CMS

--- pages/database.vue
+++ pages/database.vue
@@ -1,18 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { getSingletonItem } = useDirectusItems();
+interface IDatabase {
+  id?: string;
+  Content: string;
+  Heading: string;
+}
 
+const database = await getSingletonItem<IDatabase>({
+  collection: "Database",
+});
+</script>
 <template>
   <div class="bg-white py-6 dark:bg-gray-800 sm:py-8 lg:py-12">
     <div class="mx-auto max-w-screen-md px-4 md:px-8">
       <h1
-        class="mb-4 text-center text-2xl font-bold text-gray-800 dark:text-gray-200 sm:text-3xl md:mb-6"
+        class="mb-4 text-center text-2xl font-bold text-gray-800 dark:text-white sm:text-3xl md:mb-6"
       >
-        Database of The Seven Sages of Rome
+        {{ database.Heading }}
       </h1>
-      <p
-        class="mb-6 text-center text-gray-500 dark:text-gray-200 sm:text-lg md:mb-8"
-      >
-        Work in Progress
-      </p>
+
+      <span
+        class="mb-6 list-inside text-justify text-gray-500 dark:text-gray-200 sm:text-lg md:mb-8"
+        v-html="database.Content"
+      />
     </div>
   </div>
 </template>
