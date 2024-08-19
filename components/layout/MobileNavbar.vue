db6b1e84ffd4a56728d03271866be647a45c3d12
maxnth <maximilian.noeth@protonmail.com>
2024-08-19 18:39:05 +0200

    update navbar font size

--- components/layout/MobileNavbar.vue
+++ components/layout/MobileNavbar.vue
@@ -12,12 +12,12 @@ defineExpose({
 })
 </script>
 <template>
-  <Drawer v-model:visible="visible" class="!w-full md:!w-80 lg:!w-[30rem]">
+  <Drawer v-model:visible="visible" class="!w-full">
     <template #container="{ closeCallback }">
       <div class="flex flex-col h-full">
         <div class="flex items-center justify-between px-6 pt-4 shrink-0">
                 <span class="inline-flex items-center gap-2">
-                    <span class="font-semibold text-2xl text-primary">Seven Sages of Rome</span>
+                    <NuxtLink to="/" class="font-semibold text-3xl text-primary">Seven Sages of Rome</NuxtLink>
                 </span>
           <span>
               <Button type="button" @click="closeCallback" icon="pi pi-times" rounded outlined></Button>
@@ -30,7 +30,7 @@ defineExpose({
                 <li v-for="link in navLinks" :key="link.name">
                   <NuxtLink
                     :to="link.link"
-                    class="group text-5xl flex items-center rounded-lg p-2 text-gray-900 hover:bg-gray-100"
+                    class="group text-6xl lg:text-8 xl flex items-center rounded-lg p-2 text-gray-900 hover:bg-gray-100"
                     @click="visible = false"
                   >
                     {{ link.name }}
